273ef9947c2d0e0dd93f8af907d1f3ce5cfbf633
Behrouz <behrouzshakeri05@gmail.com>
2026-02-19 22:33:58 +0330

    feat(app): Add a header slider and add a mobile search box

--- public/src/script/shared.js
+++ public/src/script/shared.js
@@ -1,5 +1,24 @@
 import { initMobileSidebar } from "./funcs/shared.js";
+import Swiper from "../../../node_modules/swiper/swiper-bundle.min.mjs";
 window.addEventListener("DOMContentLoaded", () => {
+  var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    simulateTouch: true,
+    allowTouchMove: true,
+  });
+
   initMobileSidebar({
     sidebarSelector: "#header-mobile__sidebar",
     overlaySelector: "#sidebar-mobile__overlay",
