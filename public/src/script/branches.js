da54eb9ed43b79ce5aafca9d841c9b7112fd24d2
Behrouz <behrouzshakeri05@gmail.com>
2026-02-26 19:25:13 +0330

    feat(app): Add a comments section

--- public/src/script/branches.js
+++ public/src/script/branches.js
@@ -20,4 +20,25 @@ window.addEventListener("DOMContentLoaded", () => {
     },
     watchSlidesProgress: true,
   });
+  var swiper = new Swiper("#comments-slider", {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 16,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      loop: false,
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 24,
+      },
+    },
+    watchSlidesProgress: true,
+  });
 });
